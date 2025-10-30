3f63f23f5b0ebbd7405cbe29bf0d5c7946b5a9ab
한선호 <sunnyd@yonsei.ac.kr>
2025-10-30 16:31:45 +0900

    Implement Node struct and makeList function
    Defined a Node struct and implemented the makeList function to create a linked list from a string. Updated the main function to connect two linked lists and print them together.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,31 @@
 // TODO: Define a struct named "Node".
 // The struct should include a single character ch followed by a pointer, which will indicate the next Node.
 // Think of it as one link in a chain of characters.
+struct Node {
+    char  ch;
+    Node* next;
+    Node(char c) : ch(c), next(nullptr) {}
+};
 
 
 // TODO: Implement the makeList function.
 // It should start with empty head and tail pointers.
 // Then, go through the given string to create and connect each Node in order.
 void makeList(const char* s, Node*& head, Node*& tail) {
+    head = nullptr;
+    tail = nullptr;
 
+    if (s == nullptr) return;
+
+    for (const char* p = s; *p != '\0'; ++p) {
+        Node* node = new Node(*p);
+        if (head == nullptr) {        
+            head = tail = node;
+        } else {                      
+            tail->next = node;
+            tail = node;
+        }
+    }
 }
 
 void printList(Node* head) {
@@ -29,12 +47,16 @@ int main() {
 
     Node* nameHead = nullptr;
     Node* nameTail = nullptr;
-    makeList("MyName", nameHead, nameTail); // You can freely change the input parameter as your name.
+    makeList("Sunho", nameHead, nameTail); // You can freely change the input parameter as your name.
 
 
     // TODO: Connect the two linked lists so that printing them together
     // printList function should display "Hello {yourName}" in one line.
-
+    if (helloTail != nullptr) {
+        helloTail->next = nameHead;
+    } else {
+        helloHead = nameHead;
+    }
 
     printList(helloHead);
 
