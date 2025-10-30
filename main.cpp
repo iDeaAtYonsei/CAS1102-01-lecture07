06971f304524bdcd5ec2600c290309c60189596f
Jungwoo <pjw159@yonsei.ac.kr>
2025-10-30 15:29:48 +0900

    Building & Connecting Linked List

--- main.cpp
+++ main.cpp
@@ -4,13 +4,30 @@
 // TODO: Define a struct named "Node".
 // The struct should include a single character ch followed by a pointer, which will indicate the next Node.
 // Think of it as one link in a chain of characters.
-
+struct Node {
+    char ch;
+    Node* next = nullptr;
+};
 
 // TODO: Implement the makeList function.
 // It should start with empty head and tail pointers.
 // Then, go through the given string to create and connect each Node in order.
 void makeList(const char* s, Node*& head, Node*& tail) {
-
+    head = nullptr;
+    tail = nullptr;
+    while (*s != '\0') {
+        Node* newNode = new Node();
+        newNode->ch = *s;
+        newNode->next = nullptr;
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        s++;
+    }
 }
 
 void printList(Node* head) {
@@ -29,12 +46,16 @@ int main() {
 
     Node* nameHead = nullptr;
     Node* nameTail = nullptr;
-    makeList("MyName", nameHead, nameTail); // You can freely change the input parameter as your name.
+    makeList("Jungwoo Park", nameHead, nameTail); // You can freely change the input parameter as your name.
 
 
     // TODO: Connect the two linked lists so that printing them together
     // printList function should display "Hello {yourName}" in one line.
-
+    if (helloTail != nullptr) {
+        helloTail->next = nameHead;
+    } else {
+        helloHead = nameHead;
+    }
 
     printList(helloHead);
 
